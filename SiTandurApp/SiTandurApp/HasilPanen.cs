925434931f13adb7bcdf18a035face04fbf22715
lilypon246 <lailymedhaa.595@gmail.com>
2023-09-22 06:47:45 +0700

    feat: add HasilPanen class and its methods

---
+++ SiTandurApp/SiTandurApp/HasilPanen.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiTandurApp
+{
+    internal class HasilPanen
+    {
+        private int IDPanen;
+        private string namaTanaman;
+        private double beratTanaman;
+        private DateTime tanggalPanen;
+        private string lokasiPanen;
+        private decimal hargaPasar;
+
+        public decimal HargaJual { get; set; }
+
+        // Constructor
+        public HasilPanen(int idPanen, string namaTanaman, double beratTanaman, DateTime tanggalPanen, string lokasiPanen, decimal hargaPasar)
+        {
+            IDPanen = idPanen;
+            this.namaTanaman = namaTanaman;
+            this.beratTanaman = beratTanaman;
+            this.tanggalPanen = tanggalPanen;
+            this.lokasiPanen = lokasiPanen;
+            this.hargaPasar = hargaPasar;
+        }
+
+        public void AddHasilPanen(List<HasilPanen> hasilPanenList, HasilPanen newHasilPanen)
+        {
+            hasilPanenList.Add(newHasilPanen);
+            Console.WriteLine("Hasil panen berhasil ditambahkan!");
+        }
+
+        public void EditHasilPanen(List<HasilPanen> hasilPanenList, int targetID, HasilPanen updatedHasilPanen)
+        {
+            HasilPanen existingCropField = hasilPanenList.FirstOrDefault(c => c.IDPanen == targetID);
+            //penerapan LINQ FirstOrDefault untuk mengecek apakah ada elemen atau tidak
+
+            if (existingCropField != null)
+            {
+                // Update fitur dari hasil panen
+                existingCropField.namaTanaman = updatedHasilPanen.namaTanaman;
+                existingCropField.beratTanaman = updatedHasilPanen.beratTanaman;
+                existingCropField.tanggalPanen = updatedHasilPanen.tanggalPanen;
+                existingCropField.lokasiPanen = updatedHasilPanen.lokasiPanen;
+                existingCropField.hargaPasar = updatedHasilPanen.hargaPasar;
+                existingCropField.HargaJual = updatedHasilPanen.HargaJual;
+
+                Console.WriteLine("Hasil panen berhasil diperbarui!");
+            }
+            else
+            {
+                Console.WriteLine("Hasil panen dengan ID " + targetID + " tidak dapat ditemukan.");
+            }
+        }
+
+        public void DeleteHasilPanen(List<HasilPanen> hasilPanenList, int targetID)
+        {
+            HasilPanen cropFieldToDelete = hasilPanenList.FirstOrDefault(c => c.IDPanen == targetID);
+
+            if (cropFieldToDelete != null)
+            {
+                hasilPanenList.Remove(cropFieldToDelete);
+
+                Console.WriteLine("Hasil panen dengan ID " + targetID + " berhasil dihapus!");
+            }
+            else
+            {
+                Console.WriteLine("Hasil panen dengan ID " + targetID + " tidak dapat ditemukan.");
+            }
+        }
+    }
+}
---
+++ SiTandurApp/SiTandurApp/User.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiTandurApp
+{
+    internal class User
+    {
+        private int _userID;
+        private string _emailUser;
+        private string _passwordUser;
+        private bool _isAdmin;
+        private bool _isLoggedIn;
+
+        public int UserID 
+        { 
+            get { return _userID; } 
+        }
+
+        public string EmailUser
+        {
+            get { return _emailUser; }
+            set { _emailUser = value; }
+        }
+
+        public string PasswordUser
+        {
+            get { return _passwordUser; }
+            set { _passwordUser = value; }
+        }
+
+        public bool IsAdmin
+        {
+            get { return _isAdmin; }
+            set { _isAdmin = value; }
+        }
+
+        public bool IsLoggedIn
+        {
+            get { return _isLoggedIn; }
+            set { _isLoggedIn = value; }
+        }
+
+
+        public Boolean Login(String emailUser, String passwordUser)
+        {
+            // Ini nanti diganti menyesuaikan GUI dan Database
+            // Bau bau diganti sangat kuat wkwkkw
+
+            if (emailUser == "sayaadmin@gmail.com" & passwordUser == "qwerty")
+            {
+                _isAdmin = true;
+                return true;
+            }else if (emailUser == "sayapetani@gmail.com" & passwordUser == "nandurismylife")
+            {
+                _isAdmin = false;
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+        public void VerifyLoginStatus(String emailUser, String passwordUser)
+        {
+            IsLoggedIn = Login(emailUser, passwordUser);
+        }
+
+        public void Logout()
+        {
+            IsLoggedIn = false;
+        }
+    }
+}
