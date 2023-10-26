7256ee5fc0f1abe1c678971cb9f1438606f40b6c
Rayhan Adi Wicaksono <rayhan.a.wicaksono@gmail.com>
2023-10-26 18:35:13 +0700

    fix: minor update

--- SiTandurWPFApp/SiTandurWPFApp/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ SiTandurWPFApp/SiTandurWPFApp/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -69,7 +69,7 @@ namespace SiTandurWPFApp {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SiTandurWPFApp;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SiTandurWPFApp;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
