9e244fe3838d21a4c86266987fbdf39aef641c2f
Rayhan Adi Wicaksono <rayhan.a.wicaksono@gmail.com>
2023-10-30 21:50:18 +0700

    feat: merge daffa's xaml file into lily's

---
+++ SiTandurWPFApp/SiTandurWPFApp/AdminHapusData.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SiTandurWPFApp
+{
+    /// <summary>
+    /// Interaction logic for AdminHapusData.xaml
+    /// </summary>
+    public partial class AdminHapusData : Window
+    {
+        public AdminHapusData()
+        {
+            InitializeComponent();
+        }
+
+        private void HapusDataBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
