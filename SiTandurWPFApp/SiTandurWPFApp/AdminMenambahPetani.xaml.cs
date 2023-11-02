a96c2b02da9fe88fcb6ebfd8364e103d628f097e
Rayhan Adi Wicaksono <rayhan.a.wicaksono@gmail.com>
2023-11-02 22:55:22 +0700

    feat: add tambah petani

--- SiTandurWPFApp/SiTandurWPFApp/AdminMenambahPetani.xaml.cs
+++ SiTandurWPFApp/SiTandurWPFApp/AdminMenambahPetani.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,18 @@ namespace SiTandurWPFApp
     /// </summary>
     public partial class AdminMenambahPetani : Window
     {
+
+        public DataTable dtDataPetani;
+        public static NpgsqlCommand cmd;
+        private string sql = null;
+        private NpgsqlConnection conn;
+
         public AdminMenambahPetani()
         {
             InitializeComponent();
+
+            string connstring = "Host=localhost;port=5432;Username=adminsitandur;Password=halo123;Database=sitandur";
+            conn = new NpgsqlConnection(connstring);
         }
 
         private void AdminAddKelompokPetani_TextChanged(object sender, TextChangedEventArgs e)
@@ -31,7 +42,36 @@ namespace SiTandurWPFApp
 
         private void BtnAdminSimpanTambahPetani_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
+
+            try 
+            {
+                conn.Open();
+                string queryInsertPetani = @"select * from st_insert_petani(:_namapetani, :_emailpetani, :_alamatpetani, :_kelompoktani, :_usiapetani, :_passwordpetani)";
+                cmd = new NpgsqlCommand(queryInsertPetani, conn);
+                cmd.Parameters.AddWithValue("_namapetani", AdminAddNamaPetani.Text);
+                cmd.Parameters.AddWithValue("_emailpetani", AdminAddEmailPetani.Text);
+                cmd.Parameters.AddWithValue("_alamatpetani", AdminAddAlamatPetani.Text);
+                cmd.Parameters.AddWithValue("_kelompoktani", AdminAddKelompokPetani.Text);
+                int usiaPetani;
+                if (int.TryParse(AdminAddUsiaPetani.Text, out usiaPetani))
+                {
+                    cmd.Parameters.AddWithValue("_usiapetani", usiaPetani);
+                }
+                cmd.Parameters.AddWithValue("_passwordpetani", AdminAddPasswordPetani.Text);
+                if ((int)cmd.ExecuteScalar() == 1)
+                {
+                    MessageBox.Show("Data Petani Berhasil Diinputkan!");
+                }
+                conn.Close();
+
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
         }
 
         private void BtnAdminBatalTambahPetani_Click(object sender, RoutedEventArgs e)
