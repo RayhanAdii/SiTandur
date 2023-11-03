cb6673b1b8e26600c43347bfef2a06ee96c1497e
lilypon246 <lailymedhaa.595@gmail.com>
2023-11-03 08:19:55 +0700

    feat: add EditHasilPanen and AdminMengeditTanamanWindow (xaml)

---
+++ SiTandurWPFApp/SiTandurWPFApp/AdminMengeditTanamanWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SiTandurWPFApp
+{
+    /// <summary>
+    /// Interaction logic for AdminMengeditTanamanWindow.xaml
+    /// </summary>
+    public partial class AdminMengeditTanamanWindow : Window
+    {
+        public AdminMengeditTanamanWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAdminSimpanEditTanaman_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAdminBatalEditTanaman_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
