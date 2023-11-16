2812a2d75bab34d2b98d396f3cbb37f51eee7cb1
defz7 <daffa.kamal@mail.ugm.ac.id>
2023-11-16 23:25:32 +0700

    feat: add weather info in petani dashboard

--- SiTandurWPFApp/SiTandurWPFApp/PetaniDashboard.xaml.cs
+++ SiTandurWPFApp/SiTandurWPFApp/PetaniDashboard.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using LiveCharts;
+using Newtonsoft.Json;
 
 namespace SiTandurWPFApp
 {
@@ -40,6 +42,33 @@ namespace SiTandurWPFApp
             WelcomePage welcomePage = new WelcomePage();
             this.Close();
             welcomePage.Show();
+        }
+
+        string APIKey = "de654bfa63689994c449fc63b93ef46a";
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            getWeather();
+        }
+        private void getWeather()
+        {
+            using (WebClient web = new WebClient())
+            {
+                string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}", TbCity.Text, APIKey);
+                var json = web.DownloadString(url);
+                WeatherInfo.root Info = JsonConvert.DeserializeObject<WeatherInfo.root>(json);
+                lblConditions.Text = Info.weather[0].main;
+                lblDetail.Text = Info.weather[0].description;
+                lblSunset.Text = convertDateTime(Info.sys.sunset).ToShortTimeString();
+                lblSunrise.Text = convertDateTime(Info.sys.sunrise).ToShortTimeString();
+                lblWindSpeed.Text = Info.wind.speed.ToString();
+                lblHumidity.Text = Info.main.humidity.ToString();
+            }
+        }
+        DateTime convertDateTime(long millisec)
+        {
+            DateTime day = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            day = day.AddSeconds(millisec).ToLocalTime();
+            return day;
         }
     }
 }
--- SiTandurWPFApp/SiTandurWPFApp/obj/Debug/net6.0-windows/App.g.cs
+++ SiTandurWPFApp/SiTandurWPFApp/obj/Debug/net6.0-windows/App.g.cs
@@ -47,7 +47,7 @@ namespace SiTandurWPFApp {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.13.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace SiTandurWPFApp {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.13.0")]
         public static void Main() {
             SiTandurWPFApp.App app = new SiTandurWPFApp.App();
             app.InitializeComponent();
